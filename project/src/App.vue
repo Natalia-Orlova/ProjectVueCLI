a22a4af3f660883fe16e77d7a7b7b0c430850fae
NataliaOrlova <natakarelskaya@yandex.ru>
2023-11-24 19:05:52 +0400

    added component blog details

---
+++ project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <BlogDetails/>
+  </div>
+</template>
+
+<script>
+import BlogDetails from './components/BlogDetails.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BlogDetails
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Jost, sans-serif;
+  margin-top: 96px;
+  margin-bottom: 96px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.center {
+  width: 1200px;
+  margin: 0 auto;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
