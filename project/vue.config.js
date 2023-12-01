dccfd28be72b6f6fa4b041a368e3c70f66d38eb9
NataliaOrlova <natakarelskaya@yandex.ru>
2023-12-01 22:03:31 +0400

    added gh-pages

--- project/vue.config.js
+++ project/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: `/ProjectVueCLI/`,
+  // publicPath: `/project/`,
   transpileDependencies: true
 })
